15955b908d98e8f16d291ae2c0fc51bb701e2e6b
James Pearce <jamesquintanapearce@gmail.com>
2020-03-25 13:10:32 +1100

    Moving while checking classes functioning

--- script.js
+++ script.js
@@ -1,48 +1,34 @@
 
-var avPos = "loc-21"; // location of avatar
-// THE FLAW IS IN currentSquareClassList - it just take the first square it finds with currentClass, but it needs to check which one it is. So also need to check other row or column.
+// up and down are done, left and right are not. HOWEVER
+// avaPos resets to 15 when the loop is finished. Cannot get colCurrent, rowCurrent to not be 15
 
 $(document).ready(function () {
 
-    $(`#` + avPos).html('<img src="assets/images/avatar.png"></img>');
+    // $(`#` + avaPos).html('<img id="avaIcon" src="assets/images/avatar.png"></img>');
+
+
+    var avaPos = $('#avaIcon').parent().attr("id"); // location of avatar
+    console.log("initial location id: " + avaPos);
 
     // function to move avatar based on clicks
     $(".square").click(function (event) {
         event.stopPropagation;
         console.log($(this).attr("id") + " clicked");
+        console.log("avaPos after click: " + avaPos);
 
         // check the move is valid
-        // Split all the classes of current location into an array
-        var colCurrent = "";
-        var rowCurrent = "";
-        var currentClassList = $('#' + avPos).attr('class').split(/\s+/);
-        console.log("currentClassList: " + JSON.stringify(currentClassList));
+        var idClicked = $(this).attr("id"); // first number is column, second number is row
 
-        $.each(currentClassList, function (index, item) {
-            if (item.charAt(0) === 'c') {
-                colCurrent = item;
-            }
-            else if (item.charAt(0) === 'r') {
-                rowCurrent = item;
-            }
-        });
-        console.log("colCurrent: " + colCurrent + "||| rowCurrent: " + rowCurrent);
+        var colCurrent = avaPos.charAt(0);
+        var rowCurrent = avaPos.charAt(1);
+
+        console.log("colCurrent: " + colCurrent + " rowCurrent: " + rowCurrent + " avaPos: " + avaPos);
 
         //Split all the classes of clicked location into an array
-        var colClicked = "";
-        var rowClicked = "";
-        var clickedClassList = $(this).attr('class').split(/\s+/);
-        console.log("clickedClassList: " + JSON.stringify(clickedClassList));
+        var colClicked = idClicked.charAt(0);
+        var rowClicked = idClicked.charAt(1);
 
-        $.each(clickedClassList, function (index, item) {
-            if (item.charAt(0) === 'c') {
-                colClicked = item;
-            }
-            else if (item.charAt(0) === 'r') {
-                rowClicked = item;
-            }
-        });
-        console.log("colClicked: " + colClicked + "||| rowClicked: " + rowClicked);
+        console.log("colClicked: " + colClicked + " ||| rowClicked: " + rowClicked);
 
         // check to see if the square clicked is in a column or row of the current square
         if (colClicked === colCurrent || rowClicked === rowCurrent) {
@@ -50,39 +36,37 @@ $(document).ready(function () {
             // check if column is passable
 
             if (colClicked === colCurrent) {
-                let square1 = rowCurrent.charAt(1); // get the number part of the rows
-                let square2 = rowClicked.charAt(1);
-                console.log("squares: " + square1 + " " + square2);
-
                 // for going up
-                if (square2 < square1) {
-                    moveUp(square1, square2);
+                if (rowClicked < rowCurrent) {
+                    console.log("if then up - RowClicke " + rowClicked + "rowCurrent " + rowCurrent);
+                    moveUp(rowCurrent, rowClicked, avaPos);
                 }
                 // for going down
-                else if (square2 > square1) {
-                    moveDown(square1, square2);
+                else if (rowClicked > rowCurrent) {
+                    console.log("else if then down - RowClicke " + rowClicked + "rowCurrent " + rowCurrent);
+                    moveDown(rowCurrent, rowClicked, avaPos);
                 }
             }
             // check if row is passable
             else if (rowClicked === rowCurrent) {
-                let square1 = colCurrent.charAt(1); // get the number part of the columns
-                let square2 = colClicked.charAt(1);
-                console.log("squares: " + square1 + " " + square2);
 
                 // for going right
-                if (square2 > square1) {
-                    moveRight(square1, square2);
+                if (colClicked > colCurrent) {
+                    console.log("if then right - colClicked: " + colClicked + " colCurrent: " + colCurrent);
+                    moveRight(colCurrent, colClicked, avaPos);
                 }
-                else if (square2 < square1) {
-                    moveLeft(square1, square2);
+                //for going left
+                else if (colClicked < colCurrent) {
+                    console.log("if then left - colClicked: " + colClicked + " colCurrent: " + colCurrent);
+                    moveLeft(colCurrent, colClicked, avaPos);
                 }
             }
 
-            // $(`#` + avPos).html('');
+            // $(`#` + avaPos).html('');
 
-            // avPos = $(this).attr("id");
+            // avaPos = $(this).attr("id");
 
-            // $(`#` + avPos).html('<img src="assets/images/avatar.png"></img>');
+            // $(`#` + avaPos).html('<img id="avaIcon" src="assets/images/avatar.png"></img>');
         }
         else {
             console.log("class: " + $(this).attr("class"));
@@ -92,205 +76,130 @@ $(document).ready(function () {
 
     })
 
-    function moveUp(square1, square2) {
-        console.log("moveUpsquares: " + square1 + " " + square2);
-        for (var i = square1; i >= square2; i--) {
-            let currentSquare = "r" + i;
-            console.log("currentSquare: " + currentSquare);
-
-            var currentSquareClassList = $("." + currentSquare).attr("class").split(/\s+/);
+    function moveUp(rowCurrent, rowClicked, avaMove) {
+        console.log("moveUpsquares: " + rowCurrent + " " + rowClicked + "id: " + avaMove);
+        // for each square moving up the grid, as row decreases, check if passable. Start on current square in case of wall in that square
+        for (var i = rowCurrent; i >= rowClicked; i--) {
+            $(`#` + avaMove).html(''); // clear current avatar image
+            avaMove = avaMove.charAt(0) + i; // column doesn't change, row will be i
+            var currentSquareClassList = $("#" + avaMove).attr("class").split(/\s+/);
             console.log("currentSquareClassList: " + currentSquareClassList);
-            var colHere = "";
-            var rowHere = "";
             $.each(currentSquareClassList, function (index, item) {
                 if (item === "pit") {
                     console.log("GAME OVER");
                     return;
                 }
-                else if (item === "topWall" || i == square2) {
+                else if (item === "topWall" || i == rowClicked) {
                     console.log("journey ended");
-                    $.each(currentSquareClassList, function (index, item) {
-                        if (item.charAt(0) === 'c') {
-                            colHere = item;
-                            console.log("colHere: " + colHere);
-                        }
-                        else if (item.charAt(0) === 'r') {
-                            rowHere = item;
-                            console.log("rowHere: " + rowHere);
-                        }
 
-                    });
-                    console.log("avPos in loops: " + avPos);
-                    $(`#` + avPos).html('');
-                    avPos = $(`.${colHere}.${rowHere}`).attr("id"); // selects all elements with both classes
-                    console.log("hereID: " + JSON.stringify(avPos));
-
-                    $(`#` + avPos).html('<img src="assets/images/avatar.png"></img>');
-
+                    console.log("avaMove final up: " + avaMove);
+                    $(`#` + avaMove).html('<img id="avaIcon" src="assets/images/avatar.png"></img>');
+                    avaPos = avaMove;
                     return;
                 }
             })
         }
     }
 
-    function moveDown(square1, square2) {
-        console.log("moveDownsquares: " + square1 + " " + square2);
+    function moveDown(rowCurrent, rowClicked, avaMove) {
+        console.log("moveDownsquares: " + rowCurrent + " " + rowClicked + "id: " + avaMove);
 
-        for (var i = square1; i <= square2; i++) {
-            let currentSquare = "r" + i;
-            console.log("currentSquare: " + currentSquare);
-
-            var currentSquareClassList = $("." + currentSquare).attr("class").split(/\s+/);
+        for (var i = rowCurrent; i <= rowClicked; i++) {
+            $(`#` + avaMove).html(''); // clear current avatar image
+            avaMove = avaMove.charAt(0) + i; // column doesn't change, row will be i
+            var currentSquareClassList = $("#" + avaMove).attr("class").split(/\s+/);
             console.log("currentSquareClassList: " + currentSquareClassList);
-            var colHere = "";
-            var rowHere = "";
             $.each(currentSquareClassList, function (index, item) {
                 if (item === "pit") {
                     console.log("GAME OVER");
                     return;
                 }
                 else if (item === "topWall") {
-                    $.each(currentSquareClassList, function (index, item) {
-                        if (item.charAt(0) === 'c') {
-                            colHere = item;
-                            console.log("colHere: " + colHere);
-                        }
-                        else if (item.charAt(0) === 'r') {
-                            rowHere = 'r' + item.charAt(1) - 1; // This is because they will stop in the square above the one with the wall
-                            console.log("rowHere: " + rowHere);
-                        }
-                    });
-                    console.log("avPos in down: " + avPos);
-                    $(`#` + avPos).html('');
-                    avPos = $(`.${colHere}.${rowHere}`).attr("id"); // selects all elements with both classes
-                    console.log("hereID: " + JSON.stringify(avPos));
-
-                    $(`#` + avPos).html('<img src="assets/images/avatar.png"></img>');
+                    console.log("journey ended");
+                    avaMove = avaMove.charAt(0) + i - 1; // because it will be in the square above
+                    $(`#` + avaMove).html('<img id="avaIcon" src="assets/images/avatar.png"></img>');
+                    avaPos = avaMove;
                     return;
+
                 }
-                else if (i == square2) {
-                    $.each(currentSquareClassList, function (index, item) {
-                        if (item.charAt(0) === 'c') {
-                            colHere = item;
-                            console.log("colHere: " + colHere);
-                        }
-                        else if (item.charAt(0) === 'r') {
-                            rowHere = item;
-                            console.log("rowHere: " + rowHere);
-                        }
-
-                    });
-                    console.log("avPos in down: " + avPos);
-                    $(`#` + avPos).html('');
-                    avPos = $(`.${colHere}.${rowHere}`).attr("id"); // selects all elements with both classes
-                    console.log("hereID: " + JSON.stringify(avPos));
-
-                    $(`#` + avPos).html('<img src="assets/images/avatar.png"></img>');
+                else if (i == rowClicked) {
+                    console.log("journey ended");
+                    console.log("avaMove final down: " + avaMove);
+                    $(`#` + avaMove).html('<img id="avaIcon" src="assets/images/avatar.png"></img>');
+                    avaPos = avaMove;
                     return;
+
                 }
-
-
             });
         }
     }
 
-    function moveRight(square1, square2) {
-        console.log("MoveRightSquares: " + square1 + " " + square2);
+    function moveRight(colCurrent, colClicked, avaMove) {
 
-        for (var i = square1; i <= square2; i++) {
-            let currentSquare = "c" + i;
-            console.log("currentSquare: " + currentSquare);
+        for (var i = colCurrent; i <= colClicked; i++) {
+            $(`#` + avaMove).html(''); // clear current avatar image
+            avaMove = i + avaMove.charAt(1); // column is i, row doesn't change
+            console.log("avaMove in right for loop: " + avaMove);
 
-            var currentSquareClassList = $("." + currentSquare).attr("class").split(/\s+/);
+            var currentSquareClassList = $("#" + avaMove).attr("class").split(/\s+/);
             console.log("currentSquareClassList: " + currentSquareClassList);
-            var colHere = "";
-            var rowHere = "";
+
             $.each(currentSquareClassList, function (index, item) {
                 if (item === "pit") {
                     console.log("GAME OVER");
                     return;
                 }
                 else if (item === "leftWall") {
-                    $.each(currentSquareClassList, function (index, item) {
-                        if (item.charAt(0) === 'c') {
-                            colHere = 'c' + item.charAt(1) - 1; // this is because they will stop in the square to the left of the wall
-                            console.log("colHere: " + colHere);
-                        }
-                        else if (item.charAt(0) === 'r') {
-                            rowHere = item;
-                            console.log("rowHere: " + rowHere);
-                        }
-                    });
-                    console.log("avPos in right: " + avPos);
-                    $(`#` + avPos).html('');
-                    avPos = $(`.${colHere}.${rowHere}`).attr("id"); // selects all elements with both classes
-                    console.log("hereID: " + JSON.stringify(avPos));
 
-                    $(`#` + avPos).html('<img src="assets/images/avatar.png"></img>');
+                    console.log("journey ended");
+                    avaMove = (i - 1) + avaMove.charAt(1); // because it will be in the square on the left
+                    $(`#` + avaMove).html('<img id="avaIcon" src="assets/images/avatar.png"></img>');
+                    avaPos = avaMove;
                     return;
                 }
-                else if (i == square2) {
-                    $.each(currentSquareClassList, function (index, item) {
-                        if (item.charAt(0) === 'c') {
-                            colHere = item;
-                            console.log("colHere: " + colHere);
-                        }
-                        else if (item.charAt(0) === 'r') {
-                            rowHere = item;
-                            console.log("rowHere: " + rowHere);
-                        }
-                    });
-                    console.log("avPos in down: " + avPos);
-                    $(`#` + avPos).html('');
-                    avPos = $(`.${colHere}.${rowHere}`).attr("id"); // selects all elements with both classes
-                    console.log("hereID: " + JSON.stringify(avPos));
-
-                    $(`#` + avPos).html('<img src="assets/images/avatar.png"></img>');
+                else if (i == colClicked) {
+                    console.log("journey ended");
+                    console.log("avaMove final right: " + avaMove);
+                    $(`#` + avaMove).html('<img id="avaIcon" src="assets/images/avatar.png"></img>');
+                    avaPos = avaMove;
                     return;
                 }
+
             });
-
         }
-
     }
 
-    function moveLeft(square1, square2) {
-        console.log("moveLeftSquares: " + square1 + " " + square2);
-        for (var i = square1; i >= square2; i--) {
-            let currentSquare = "c" + i;
-            console.log("currentSquare: " + currentSquare);
-            var currentSquareClassList = $("." + currentSquare).attr("class").split(/\s+/);
+    function moveLeft(colCurrent, colClicked, avaMove) {
+
+        for (var i = colCurrent; i >= colClicked; i--) {
+            $(`#` + avaMove).html(''); // clear current avatar image
+            avaMove = i + avaMove.charAt(1); // column is i, row doesn't change
+
+            var currentSquareClassList = $("#" + avaMove).attr("class").split(/\s+/);
             console.log("currentSquareClassList: " + currentSquareClassList);
-            var colHere = "";
-            var rowHere = "";
             $.each(currentSquareClassList, function (index, item) {
                 if (item === "pit") {
                     console.log("GAME OVER");
                     return;
                 }
-                else if (item === "leftWall" || i === square2) {
-                    console.log("journey ended");
-                    $.each(currentSquareClassList, function (index, item) {
-                        if (item.charAt(0) === 'c') {
-                            colHere = item;
-                            console.log("colHere: " + colHere);
-                        }
-                        else if (item.charAt(0) === 'r') {
-                            rowHere = item;
-                            console.log("rowHere: " + rowHere);
-                        }
-                    });
-                    console.log("avPos in loops: " + avPos);
-                    $(`#` + avPos).html('');
-                    avPos = $(`.${colHere}.${rowHere}`).attr("id"); // selects all elements with both classes
-                    console.log("hereID: " + JSON.stringify(avPos));
-                    $(`#` + avPos).html('<img src="assets/images/avatar.png"></img>');
+                else if (item === "leftWall") {
 
+                    console.log("journey ended");
+                    $(`#` + avaMove).html('<img id="avaIcon" src="assets/images/avatar.png"></img>');
+                    avaPos = avaMove;
                     return;
                 }
-            })
+                else if (i == colClicked) {
+                    console.log("journey ended");
+                    console.log("avaMove final right: " + avaMove);
+                    $(`#` + avaMove).html('<img id="avaIcon" src="assets/images/avatar.png"></img>');
+                    avaPos = avaMove;
+                    return;
+                }
+            });
         }
     }
+
 
 
 });
