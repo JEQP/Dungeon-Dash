371137f0ea5fedfa0b0b7f393b065961b4d52523
James Pearce <jamesquintanapearce@gmail.com>
2020-04-28 17:12:02 +1000

    features and monster added to createMap

--- client/src/components/CreatePuzzleLogic/index.js
+++ client/src/components/CreatePuzzleLogic/index.js
@@ -9,7 +9,7 @@ class CreatePuzzleLogic extends Component {
         super(props);
 
         this.state = {
-            map: BlankMap,
+            dungeon: BlankMap,
             selectedFeature: "",
             clickedSquare: ""
         }
@@ -18,10 +18,11 @@ class CreatePuzzleLogic extends Component {
     setClickedSquare = (props) => {
         let clickedID = props;
         console.log("props in setClickedSquare: ", props);
-
-        this.setState({
-            clickedSquare: clickedID,
-        })
+        if (this.state.selectedFeature !== "") {
+            this.setState({
+                clickedSquare: clickedID,
+            })
+        }
     }
 
     setSelectedFeature = (props) => {
@@ -33,17 +34,103 @@ class CreatePuzzleLogic extends Component {
         })
     }
 
+
+    // take selectedFeature and clickedSquare from state. If they are both non-blank, update state based on these
+    componentDidUpdate() {
+        let selectedFeature = this.state.selectedFeature;
+        let clickedSquare = this.state.clickedSquare;
+        if (selectedFeature !== "" && clickedSquare !== "") {
+
+            let dungeon = this.state.dungeon;
+            console.log("dungeon: ", dungeon);
+            console.log("selected feature: ", selectedFeature);
+            console.log("clickedSquare: ", clickedSquare);
+
+            let colClicked = parseInt(clickedSquare.charAt(0), 10);
+            let rowClicked = parseInt(clickedSquare.charAt(1), 10);
+            let squareLeftIndex = dungeon.squareList.findIndex((element, index) => element.id === `${(colClicked - 1).toString()}${rowClicked.toString()}`);
+            let squareAboveIndex = dungeon.squareList.findIndex((element, index) => element.id === `${(colClicked).toString()}${(rowClicked - 1).toString()}`);
+            let squareIndex = dungeon.squareList.findIndex((element, index) => element.id === clickedSquare);
+
+            if (selectedFeature === "pitWallsNone") {
+                dungeon.squareList[squareIndex].classList = "square pitWallsNone";
+                dungeon.squareList[squareIndex].pit = true;
+            } else if (selectedFeature === "pitWallLeft") {
+                dungeon.squareList[squareIndex].classList = "square pitWallLeft";
+                dungeon.squareList[squareIndex].pit = true;
+                dungeon.squareList[squareIndex].leftwall = true;
+                if (colClicked > 1) {
+                    dungeon.squareList[squareLeftIndex].rightwall = true;
+                }
+            } else if (selectedFeature === "pitWallLeftTop") {
+                dungeon.squareList[squareIndex].classList = "square pitWallLeftTop";
+                dungeon.squareList[squareIndex].pit = true;
+                dungeon.squareList[squareIndex].leftwall = true;
+                dungeon.squareList[squareIndex].topwall = true;
+                if (colClicked > 1) {
+                    dungeon.squareList[squareLeftIndex].rightwall = true;
+                }
+                if (rowClicked > 1) {
+                    dungeon.squareList[squareAboveIndex].bottomwall = true;
+                }
+            } else if (selectedFeature === "pitWallTop") {
+                dungeon.squareList[squareIndex].classList = "square pitWallTop";
+                dungeon.squareList[squareIndex].pit = true;
+                dungeon.squareList[squareIndex].topwall = true;
+                if (rowClicked > 1) {
+                    dungeon.squareList[squareAboveIndex].bottomwall = true;
+                }
+            } else if (selectedFeature === "wallLeft") {
+                dungeon.squareList[squareIndex].classList = "square wallLeft";
+                dungeon.squareList[squareIndex].leftwall = true;
+                if (colClicked > 1) {
+                    dungeon.squareList[squareLeftIndex].rightwall = true;
+                }
+            } else if (selectedFeature === "wallLeftTop") {
+                dungeon.squareList[squareIndex].classList = "square wallLeftTop";
+                dungeon.squareList[squareIndex].leftwall = true;
+                dungeon.squareList[squareIndex].topwall = true;
+                if (colClicked > 1) {
+                    dungeon.squareList[squareLeftIndex].rightwall = true;
+                }
+                if (rowClicked > 1) {
+                    dungeon.squareList[squareAboveIndex].bottomwall = true;
+                }
+            } else if (selectedFeature === "wallTop") {
+                dungeon.squareList[squareIndex].classList = "square wallTop";
+                dungeon.squareList[squareIndex].topwall = true;
+                if (rowClicked > 1) {
+                    dungeon.squareList[squareAboveIndex].bottomwall = true;
+                }
+
+            } else if (selectedFeature === "monster") {
+                dungeon.squareList[squareIndex].monster = true;
+            }
+
+            selectedFeature = "";
+            clickedSquare = "";
+            this.setState({
+                clickedSquare: "",
+                selectedFeature: ""
+            })
+                        console.log("selected feature: ", selectedFeature);
+            console.log("clickedSquare: ", clickedSquare);
+        }
+    }
+
+
+
     render() {
         console.log("createPuzzleLogic state: ", this.state);
         return (
             <section>
-            <CreateMapOptions 
-            setSelectedFeature={this.setSelectedFeature}
-            />
-            <GameGrid
-                setClickedSquare={this.setClickedSquare}
-                squareList={this.state.map.squareList}
-            />
+                <CreateMapOptions
+                    setSelectedFeature={this.setSelectedFeature}
+                />
+                <GameGrid
+                    setClickedSquare={this.setClickedSquare}
+                    squareList={this.state.dungeon.squareList}
+                />
             </section>
         );
     }
