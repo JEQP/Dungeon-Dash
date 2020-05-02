e4ed96415ee4feb2f6e4d74a0e200cbedf704d7b
James Pearce <jamesquintanapearce@gmail.com>
2020-05-02 12:15:28 +1000

    db routes

--- client/src/config/keys.js
+++ client/src/config/keys.js
@@ -1,5 +1,5 @@
-
 module.exports = {
-    mongoURI: "mongodb+srv://jeqp:eU0Y9I2pEQCkfYVO@cluster0-2fjyo.mongodb.net/dungeon-dash?retryWrites=true&w=majority",
+    mongoURI: process.env.MONGODB_URI || "mongodb+srv://jeqp:eU0Y9I2pEQCkfYVO@cluster0-2fjyo.mongodb.net/dungeon-dash?retryWrites=true&w=majority",
     secretOrKey: "secret" 
   };
+ 
