60adc90430bdb696d77ff10dfea1eda64befc110
James Pearce <jamesquintanapearce@gmail.com>
2020-07-17 18:03:51 +1000

    adjusted css for navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -5,10 +5,10 @@ import { Link } from "react-router-dom";
 
 function Navbar(props) {
     return (
-        <div>
-             <Link to="/play" className="link-text"><Button variant="primary"  >Play Now</Button></Link>{' '}
-             <Link to="/create" className="link-text"><Button variant="success"  >Create Dungeon</Button></Link>{' '}
-             <Link to="/stadium" className="link-text"><Button variant="warning" >Enter Stadium</Button></Link>{' '}
+        <div className="navbar-layout">
+             <Link to="/play" className="link-text" id="pn"><Button variant="primary"  >Play Now</Button></Link>{' '}
+             <Link to="/create" className="link-text" id="cd"><Button variant="success"  >Create Dungeon</Button></Link>{' '}
+             <Link to="/stadium" className="link-text" id="es"><Button variant="warning" >Enter Stadium</Button></Link>{' '}
         </div>
     );
 }
